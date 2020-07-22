9998c5ef5bbef8146f15d65ae993be6a445388f0
Raul Sfirlogea <rrry3k@gmail.com>
2020-07-22 18:17:26 +0200

    Initial Commit

---
+++ CoverterUnits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoverterUnits
+//
+//  Created by Raul Sfirlogea on 22/07/2020.
+//  Copyright © 2020 Raul Sfirlogea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
